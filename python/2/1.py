319ccc52487481e60ed4751fc3b9a7c3031d9660
jyht <304931037@qq.com>
2020-07-21 21:46:17 +0800

    3
    1

--- python/2/1.py
+++ python/2/1.py
@@ -7,7 +7,7 @@ DG = nx.DiGraph()
 #一次性添加多节点，输入的格式为列表 
 DG.add_nodes_from(['A', 'B', 'C', 'D']) 
 #添加边，数据格式为列表 
-DG.add_edges_from([('A', 'B'), ('A', 'C'), ('A', 'D'), ('D','A'),('C','A'),('C','A')]) 
+DG.add_edges_from([('A', 'B'), ('A', 'C'), ('A', 'D'), ('D','A'),('C','A'),('C','A'),('D','F')]) 
 #作图，设置节点名显示,节点大小，节点颜色 
 nx.draw(DG,with_labels=True, node_size=900, node_color = colors) 
 plt.show() 
