271b8115ca96608994f012abc44fe65d8fb74196
jyht <304931037@qq.com>
2020-07-21 20:35:21 +0800

    进度条
    进度条

--- python/2/3.py
+++ python/2/3.py
@@ -1,7 +1,6 @@
-from time import sleep
 from tqdm import tqdm
-# 这里同样的，tqdm就是这个进度条最常用的一个方法
-# 里面存一个可迭代对象
-for i in tqdm(range(1, 500)):
-   # 模拟你的任务
-   sleep(0.01)
+import time
+ 
+for i in tqdm(range(100)):
+  time.sleep(0.1)
+  pass
