f20646f58fd839232eafaa361a9136dd0785af43
jyht <304931037@qq.com>
2020-07-22 09:39:36 +0800

    Create 2.py

---
+++ 2/2.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+#https://blog.csdn.net/qq_31456593/article/details/88377117
+print(tf.__version__)
+print(tf.keras.__version__)
+# 2.2.0 2.3.0-tf
